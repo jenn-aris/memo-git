89cf102b9cf04c058207dc000020047a96083fa4
Jennaris <jennaris@pop-os.localdomain>
2020-09-25 18:08:09 +0200

    changes files

---
+++ precache-manifest.4879ae25bf510c12c26b4b67533a6ac1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9566b88b011b1fab4c91b88e1847dafc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f72594adf5ac7eb351d2",
+    "url": "/static/css/main.ae62a670.chunk.css"
+  },
+  {
+    "revision": "712de2161fadc71be331",
+    "url": "/static/js/2.4750296a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4750296a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f72594adf5ac7eb351d2",
+    "url": "/static/js/main.7ddead24.chunk.js"
+  },
+  {
+    "revision": "c9ddcb2ce18bc87e6248",
+    "url": "/static/js/runtime-main.faee98fd.js"
+  }
+]);
